0824ee7e22df42888ebabc32de764d3b89aa806a
Srinath <srinathv1001@gmail.com>
2021-11-06 00:49:47 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+import os
+import handshape_feature_extractor 
+from handshape_feature_extractor import HandShapeFeatureExtractor
+from frameextractor import frameExtractor
+from scipy import spatial
+import csv
+from csv import reader
+
+middle_frames = []
+extractor = HandShapeFeatureExtractor()
+
+with open('train_frames.csv', 'r') as read_obj:
+	csv_reader = reader(read_obj)
+	for row in csv_reader:
+		row = np.array([float(x) for x in row])
+		middle_frames.append(row)
+middle_frames = np.array(middle_frames)
+
+
+outputs = []
+with open('train_outputs.csv', 'r') as read_obj:
+	csv_reader = reader(read_obj)
+	for row in csv_reader:
+		outputs.append(int(row[0]))
+outputs = np.array(outputs)
+
+results = []
+
+gnames = os.listdir("test")
+for gname in gnames:
+	path = os.path.join("test",gname)
+	frames = frameExtractor(path)
+	fvect = []
+	for frame in frames:
+		fvect.extend(extractor.extract_feature(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))[0])
+	mindisti, mindist = 0,1000
+	cc=0
+	for v in middle_frames:
+		cosdist = spatial.distance.cosine(fvect,v)
+		if cosdist<mindist:
+			mindist = cosdist
+			mindisti = cc
+		cc+=1
+	results.append([ outputs[mindisti] ])
+filename = "results.csv"
+with open(filename, 'w') as csvfile: 
+	csvwriter = csv.writer(csvfile)
+	csvwriter.writerows(results)
---
+++ main_train.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 28 00:44:25 2021
+
+@author: chakati
+"""
+from frameextractor import frameExtractor
+import cv2
+import csv
+import numpy as np
+import os
+import handshape_feature_extractor 
+from handshape_feature_extractor import HandShapeFeatureExtractor
+
+middle_frames = []
+outputs = []
+
+gestID = {}
+for x in range(10):
+	gestID["Num"+str(x)] = x
+gestID["FanDown"] = 10
+gestID["FanOn"] = 11
+gestID["FanOff"] = 12
+gestID["FanUp"] = 13
+gestID["LightOff"] = 14
+gestID["LightOn"] = 15
+gestID["SetThermo"] = 16
+
+test_middle_frames = []
+test_outputs = []
+
+extractor = HandShapeFeatureExtractor()
+print(extractor.model.summary())
+
+gnames = os.listdir("traindata")
+for gname in gnames:
+	print(gname)
+	gnameids = os.listdir("traindata/"+gname)
+	# for x in range(2):
+	for gnameid in gnameids:
+		fvect=[]
+		path = "traindata/"+gname+"/"+gnameid #str(x)+"_Venkobarao.mp4"
+		frames = frameExtractor(path)
+		for frame in frames:
+			fvect.extend(extractor.extract_feature(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))[0])
+		middle_frames.append(np.array(fvect))
+		print(len(fvect))
+		outputs.append([gestID[gname]])
+
+middle_frames = np.array(middle_frames)
+outputs = np.array(outputs)
+filename = "train_frames.csv"
+with open(filename, 'w') as csvfile: 
+	csvwriter = csv.writer(csvfile)
+	csvwriter.writerows(middle_frames)
+
+filename = "train_outputs.csv"
+with open(filename, 'w') as csvfile: 
+	csvwriter = csv.writer(csvfile)
+	csvwriter.writerows(outputs)
+
+print(middle_frames.shape, outputs.shape)
