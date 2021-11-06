3a62fa820716476426d6409a80295b8dc9afd63f
Srinath <srinathv1001@gmail.com>
2021-11-06 16:20:34 -0700

    traindata gradescope non-modified please work

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import glob
 import os
 import numpy as np
 from handshape_feature_extractor import HandShapeFeatureExtractor
@@ -7,44 +8,76 @@ from scipy import spatial
 import csv
 from csv import reader
 
-middle_frames = []
 extractor = HandShapeFeatureExtractor().get_instance()
-gnames = sorted(os.listdir("traindata/"))
+
+# =============================================================================
+# Get the penultimate layer for trainig data
+# =============================================================================
+
+trainingDataCSV = "trainingData.csv"
+testingDataCSV = "testingData.csv"
+
+'''
+
+train_middle_penult = []
+gnames = sorted(glob.glob("traindata/*.mp4"))
 c=0
-for gname in gnames:
-	# print(gname)
-	if ".mp4" not in gname:
-		continue
-	path = os.path.join("traindata",gname)
+for path in gnames:
+	print(path)
 	frameExtractor(path,"trainframes",c)
 	img = cv2.imread("trainframes" + "/%#05d.png" % (c+1))
 	frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	fvect = np.squeeze(extractor.extract_feature(frame))
-	middle_frames.append(fvect)
+	train_middle_penult.append(fvect)
 	c+=1
-results = []
-print(len(middle_frames))
-gnames = os.listdir("test/")
+
+np.savetxt(trainingDataCSV,train_middle_penult,delimiter=",")
+
+
+'''
+
+# =============================================================================
+# Get the penultimate layer for test data
+# =============================================================================
+
+test_middle_penult = []
+gnames = glob.glob("test/*.mp4")
 c=0
-for gname in gnames:
-	path = os.path.join("test",gname)
+for path in gnames:
+	print(path)
 	frameExtractor(path,"testframes",c)
 	img = cv2.imread("testframes" + "/%#05d.png" % (c+1))
 	frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	fvect = np.squeeze(extractor.extract_feature(frame).squeeze())
-	cc=0
-	mindisti, mindist = 0,1e8
-	for v in middle_frames:
-		cosdist = spatial.distance.cosine(fvect,v)
-		if cosdist<mindist:
-			mindist = cosdist
-			mindisti = cc
-		cc+=1
-	print(gname, mindisti)
-	results.append([ mindisti ])
-	c+=1
+	test_middle_penult.append(fvect)
 
+np.savetxt(testingDataCSV,test_middle_penult,delimiter=",")
+
+# =============================================================================
+# Recognize the gesture (use cosine similarity for comparing the vectors)
+# =============================================================================
+
+trainingData = np.genfromtxt(trainingDataCSV,delimiter=",")
+testData = np.genfromtxt(testingDataCSV,delimiter=",")
+
+if len(testData.shape)==1:
+	testData = np.array([testData])
+
+result = []
+indices = []
+
+def calcGestNum(trainv, testv):
+	cossimlist = []
+	for trv in trainv:
+		cossimlist.append(spatial.distance.cosine(testv, trv))
+	return cossimlist.index(min(cossimlist))
+
+for test in testData:
+	vidin = calcGestNum(trainingData,test)
+	result.append(int(vidin))
 filename = "results.csv"
-with open(filename, 'w') as csvfile: 
-	csvwriter = csv.writer(csvfile)
-	csvwriter.writerows(results)
+np.savetxt(filename,result,delimiter=",",fmt="% d")
+# with open(filename, 'w') as csvfile: 
+# 	csvwriter = csv.writer(csvfile)
+# 	csvwriter.writerows(results)
+
